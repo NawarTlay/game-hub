248a6393d397c90c27fb0ff94798306c6c9df0c7
NawarTlay <tlynwar6@gmail.com>
2024-10-05 20:12:01 +0300

    create a custom hook to fetch the games

--- src/hooks/useGAmes.ts
+++ src/hooks/useGAmes.ts
@@ -27,7 +27,7 @@ const useGames = () => {
                 setErorr(err.message)
             });
         
-        return controller.abort();
+        return () => controller.abort();
     }, []);
 
     return { games, error };
