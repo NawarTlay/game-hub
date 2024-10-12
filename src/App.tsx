022eec73f15b4a71e2304a3338321257545783cc
NawarTlay <tlynwar6@gmail.com>
2024-10-12 05:03:02 +0300

    Add a dynamic page heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, HStack, Show } from "@chakra-ui/react"
+import { Box, Grid, GridItem, HStack, Show } from "@chakra-ui/react"
 import NavBar from "./components/NavBar"
 import GameGrid from "./components/GameGrid"
 import GenreList from "./components/GenreList"
@@ -7,6 +7,7 @@ import { Genre } from "./hooks/useGenres"
 import PlatformSelector from "./components/PlatformSelector"
 import { Platform } from "./hooks/useGAmes"
 import SortSelector from "./components/SortSelector"
+import GameHeading from "./components/GameHeading"
 
 export interface GameQuery {
   genre: Genre | null;
@@ -29,7 +30,7 @@ function App() {
     }}
   >
     <GridItem area='nav'> 
-      <NavBar onSearch={(searchText) => setGameQuery({...gameQuery, searchText})} />
+      <NavBar onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })} />
     </GridItem>
     <Show above="lg">
       <GridItem area='aside' paddingX={5}>
@@ -37,10 +38,13 @@ function App() {
       </GridItem>
     </Show>
     <GridItem area='main'>
-      <HStack marginBottom={5} spacing={5} marginLeft={2}>
-        <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform) => setGameQuery({ ...gameQuery, platform })} />
-        <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({ ...gameQuery, sortOrder })}/>
-      </HStack>
+      <Box marginLeft={2}>
+        <GameHeading gameQuery={gameQuery} />
+        <HStack marginBottom={5} spacing={5}>
+          <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform) => setGameQuery({ ...gameQuery, platform })} />
+          <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({ ...gameQuery, sortOrder })}/>
+        </HStack>
+      </Box>
       <GameGrid gameQuery={gameQuery} />
     </GridItem>  
   </Grid>  
